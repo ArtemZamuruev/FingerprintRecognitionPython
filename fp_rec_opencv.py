544307f93b6cc4ab99c8a8272bda231859e2563c
Artem Zamuruev <artem.zamuruev@gmail.com>
2017-05-24 17:05:54 +0300

    Added a new approximative adaptive binarization fuction. Also added a functionality to hand images sorting. When looping images press G to mark it as good and B to mark image as bad and then run the delbad.sh script

--- fp_rec_opencv.py
+++ fp_rec_opencv.py
@@ -10,20 +10,21 @@ def openImagesInFolder(folder):
                  for fname in listdir(folder)
                  if isfile(join(folder, fname))]
     try:
-        return [cv.imread(img_path) for img_path in img_paths]
+        return [(cv.imread(img_path), img_path) for img_path in img_paths]
     except Exception:
         print "Something went wrong during opening images in folder: %s" % folder
         return False
 
 
-def showImage(img, winname="Fingerprints", trackbar=None):
+def showImage(img, winname="Fingerprints", trackbars=None):
     cv.imshow(winname, img)
-    if trackbar is not None:
-        cv.createTrackbar(trackbar["Name"],
-                          winname,
-                          trackbar["Value"],
-                          trackbar["Count"],
-                          trackbar["Update"])
+    if trackbars is not None:
+        for tr in trackbars:
+            cv.createTrackbar(tr["Name"],
+                              winname,
+                              tr["Value"],
+                              tr["Count"],
+                              tr["Update"])
 
 
 def getKey(delay=0):
@@ -57,8 +58,15 @@ def imgToGray(img):
     return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 
-def applyMedianFilter(img, kernsize=5):
-    return cv.medianBlur(img, kernsize)
+def applyBlur(img, method="GAUSSIAN", kernsize=7, sigma=2):
+    result = None
+    if method == "GAUSSIAN":
+        result = cv.GaussianBlur(img, (kernsize, kernsize), sigma)
+    elif method == "MEDIAN":
+        result = cv.medianBlur(img, kernsize)
+    else:
+        return False
+    return result
 
 
 def otsuThreshold(img):
@@ -66,29 +74,27 @@ def otsuThreshold(img):
     return thr
 
 
-def binaryThreshold(img, thresh=100):
-    return cv.threshold(img, thresh, 255, cv.THRESH_BINARY)[1]
-
-
-def adaptiveThreshold_1(img):
-    thr = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-    return thr
-
-
-def adaptiveThreshold_2(img):
-    return cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-
-
-def sobelFilter(img):
-    return (cv.Sobel(img, cv.CV_8U, 1, 0), cv.Sobel(img, cv.CV_8U, 0, 1))
-
-def addTwoImages(img1, img2, alpha):
-    beta = float(1 - alpha)
-    return cv.addWeighted(img1, alpha, img2, beta, 0)
-
-
-def equalHist(img):
-    return cv.equalizeHist(img)
+def adaptiveThreshold(img, method="MEAN", bsize=9, c=1):
+    result = None
+    if method == "GAUSSIAN":
+        result = cv.adaptiveThreshold(
+            img,
+            255,
+            cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv.THRESH_BINARY,
+            bsize,
+            c)
+    elif method == "MEAN":
+        result = cv.adaptiveThreshold(
+            img,
+            255,
+            cv.ADAPTIVE_THRESH_MEAN_C,
+            cv.THRESH_BINARY,
+            bsize,
+            c)
+    else:
+        return False
+    return result
 
 
 def sharpenImage(img, kcenter=9):
@@ -100,50 +106,52 @@ def sharpenImage(img, kcenter=9):
     return cv.filter2D(img, cv.CV_8U, kernel)
 
 
-def smoothUpdate(x):
-    global grayed
-    track_position = int(
-        cv.getTrackbarPos(
-            "Kernel Size",
-            "Smoothed"
-        )
-    )
+def sumImages(images):
+    res = images[0]
+    for i in range(1, len(images)):
+        res = addTwoImages(res, images[i])
+    return res
 
-    if track_position < 0:
-        return
 
-    newKernelSize = track_position * 2 + 1
-    newSmoothedImg = applyMedianFilter(grayed, newKernelSize)
-    showImage(newSmoothedImg, winname="Smoothed")
-    newBinarized = adaptiveThreshold_1(newSmoothedImg)
-    showImage(newBinarized, winname="Adaptive 1")
-    newBinarized_2 = adaptiveThreshold_2(newSmoothedImg)
-    showImage(newBinarized_2, winname="Adaptive 2")
+def addTwoImages(img1, img2, alpha=0.5):
+    beta = 1.0 - alpha
+    return cv.addWeighted(img1, alpha, img2, beta, 0)
 
-def thresholdUpdate(x):
-    global hist
-    track_position = int(
-        cv.getTrackbarPos(
-            "Threshold",
-            "Threshold Binarized"
-        )
-    )
 
-    if track_position < 0:
-        return
+def adaptiveBinarization(img):
+    adapt_thresholds = [
+        adaptiveThreshold(
+            img,
+            method="MEAN",
+            bsize=9,
+            c=i)
+        for i in range(0, 4)]
 
-    newBinarizedThreshold = binaryThreshold(
-        hist,
-        thresh=track_position
-    )
-    showImage(newBinarizedThreshold, winname="Threshold Binarized")
+    approx_res = sumImages(adapt_thresholds)
+
+    approx_blured = applyBlur(
+        approx_res,
+        method="GAUSSIAN",
+        kernsize=5,
+        sigma=3.3)
+
+    res_threshold = otsuThreshold(approx_blured)
+    return res_threshold
 
 
 # Make a list of images stored in given folder
 imgs = openImagesInFolder("./Fingerprints/")
 
+
+# Files for sotring pathes to
+# good and bad examples of fingerprints
+good_imgs = open("good.txt", 'w')
+bad_imgs = open("bad.txt", 'w')
+
 # Loop this images
-for img in imgs:
+for img_t in imgs:
+
+    img = img_t[0]
 
     # Show original image to comapare transitions
     showImage(img, winname="Original")
@@ -151,58 +159,31 @@ for img in imgs:
     # Convert image to grayscale
     grayed = imgToGray(img)
 
-    # Use histogram equalization:
-    # hist = equalHist(grayed)
-    # showImage(hist, winname="Histogram")
+    # Blur it to remove some noise
+    blured = applyBlur(grayed, kernsize=5, sigma=3.4)
 
-    # sharpened = sharpenImage(grayed)
-    # showImage(sharpened, winname="Sharpened")
-
-    # Apply sobel filter:
-    # sobeled = sobelFilter(hist)
-    # showImage(sobeled[0], winname="Sobeled X")
-    # showImage(sobeled[1], winname="Sobeled Y")
-    # showImage(addTwoImages(sobeled[0], sobeled[1], 0.5), winname="Sobeled Together")
-
-    # Apply median filter to smooth the image
-    smoothed = applyMedianFilter(grayed, kernsize=3)
-    smoothTrackbar = {
-        "Name": "Kernel Size",
-        "Value": 1,
-        "Count": 5,
-        "Update": smoothUpdate
-    }
-    showImage(smoothed, winname="Smoothed", trackbar=smoothTrackbar)
-
-    # Binarization
-    # binarized_otsu = otsuThreshold(hist)
-    # showImage(binarized_otsu, winname="OTSU Binarized")
-
-    thr_1 = adaptiveThreshold_1(smoothed)
-    showImage(thr_1, winname="Adaptive 1")
-
-    thr_2 = adaptiveThreshold_2(smoothed)
-    showImage(thr_2, winname="Adaptive 2")
-
-
-    # sobeled2 = addTwoImages(sobeled[0], sobeled[1], 0.5)
-    # binotsu2 = otsuThreshold(sobeled2)
-    # showImage(binotsu2, winname="Fffff")
-
-
-    # canny = cv.Canny(grayed, 100, 200)
-    # showImage(canny, winname="Canny")
-
-    # binarized_threshold = binaryThreshold(hist, thresh=100)
-    # thresholdTrackbar = {
-    #     "Name": "Threshold",
-    #     "Value": 100,
-    #     "Count": 255,
-    #     "Update": thresholdUpdate
-    # }
-    # showImage(binarized_threshold,
-    #           winname="Threshold Binarized",
-    #           trackbar=thresholdTrackbar)
+    # Binarize image
+    binarized = adaptiveBinarization(blured)
+    showImage(binarized, winname="Binarized")
 
     # Wait for any key before switch to another image
-    getKey()
+    k_code = getKey()
+
+    # G key was pressed:
+    if k_code == 103:
+        try:
+            good_imgs.write(img_t[1] + "\n")
+        except Exception:
+            print "Error while writing GOOD image path"
+            continue
+    # B key was pressed:
+    elif k_code == 98:
+        try:
+            bad_imgs.write(img_t[1] + "\n")
+        except Exception:
+            print "Erorr while writing BAD image path"
+    # ESC was pressed:
+    elif k_code == 27:
+        break
+    else:
+        continue
