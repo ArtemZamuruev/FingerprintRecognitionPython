d5bc2e0bb7c567cb3e98de7076dbea2e5a64f8b4
Artem Zamuruev <artem.zamuruev@gmail.com>
2017-05-29 21:55:03 +0300

    Beta

--- fingerprint_rec.py
+++ fingerprint_rec.py
@@ -63,11 +63,11 @@ def imgToGray(img):
 
 def getBlockVariation(block):
     summa = 0
-    mean = np.sum(block) / block.size
+    mean = float(np.sum(block)) / float(block.size)
     for r in range(block.shape[0]):
         for c in range(block.shape[1]):
-            summa += ((block[r, c] - mean) ** 2)
-    return summa / block.size
+            summa += ((float(block[r, c]) - mean) ** 2)
+    return float(summa) / float(block.size)
 
 
 def segmentation(img):
@@ -194,9 +194,6 @@ def normalization(img, d_mean=100, d_var=255):
 def computeOrientationAngle(gr_x, gr_y):
     brx, bcx = gr_x.shape
 
-    vx_ij = 0
-    vy_ij = 0
-
     g_xx = 0.0
     g_xy = 0.0
     g_yy = 0.0
@@ -209,14 +206,20 @@ def computeOrientationAngle(gr_x, gr_y):
             g_yy += float(gr_y[u, v]) * float(gr_y[u, v])
 
     ksi = float(np.arctan2(float(2.0 * g_xy), float(g_xx - g_yy))) / 2.0
-    #ksi += np.pi / 2.0
+    # ksi += np.pi / 2.0
 
     return ksi
 
 
-def orientionalComputing(img, b_shape=(15, 15)):
-    img = cv.resize(img, (img.shape[1]*2, img.shape[0]*2), interpolation=cv.INTER_CUBIC)
-    img = np.bitwise_not(img)
+def orientionalComputing(img, b_shape=(15, 15), step=None):
+
+    print "Stage:\tComputing ridge angles"
+
+    if step is None:
+        step = max(b_shape)
+
+    # img = cv.resize(img, (img.shape[1]*2, img.shape[0] * 2), interpolation=cv.INTER_CUBIC)
+    # img = np.bitwise_not(img)
     img64 = np.float64(img)
 
     sobel_x = cv.Sobel(img64, cv.CV_64F, 1, 0)
@@ -227,43 +230,47 @@ def orientionalComputing(img, b_shape=(15, 15)):
     br, bc = b_shape
     rc, cc = img64.shape
 
-    for i in range(br / 2, rc - br / 2, br):
-        for j in range(bc / 2, cc - bc / 2, bc):
+    for i in range(br / 2, rc - br / 2, step):
+        for j in range(bc / 2, cc - bc / 2, step):
 
             row_r = (i - br / 2, i + br / 2 + 1)
             col_r = (j - bc / 2, j + bc / 2 + 1)
 
-            block_x = sobel_x[row_r[0]:row_r[1] , col_r[0]:col_r[1]]
-            block_y = sobel_y[row_r[0]:row_r[1] , col_r[0]:col_r[1]]
+            block_x = sobel_x[row_r[0]:row_r[1], col_r[0]:col_r[1]]
+            block_y = sobel_y[row_r[0]:row_r[1], col_r[0]:col_r[1]]
 
             orient_mask[i, j] = computeOrientationAngle(block_x, block_y)
 
-    new_img = np.zeros(img64.shape)
-    #new_img = np.copy(img)
-    #new_img = cv.cvtColor(new_img, cv.COLOR_GRAY2BGR)
+    # new_img = np.zeros(img64.shape)
+    # new_img = np.copy(img)
+    # new_img = cv.cvtColor(new_img, cv.COLOR_GRAY2BGR)
 
-    for r in range(rc):
-        for c in range(cc):
-            if orient_mask[r, c] != 0:
-                drawLineFromPoint(new_img, (c, r), orient_mask[r, c], 4, two_dir=True)
-                
-    showImage(np.concatenate((sobel_x, sobel_y, orient_mask, new_img), axis=1), winname="Sobels")
+    # for r in range(rc):
+    #     for c in range(cc):
+    #         if orient_mask[r, c] != 0:
+    #             drawLineFromPoint(new_img, (c, r), orient_mask[r, c], 4, two_dir=True)
+
+    # showImage(np.concatenate((sobel_x, sobel_y, orient_mask, new_img), axis=1), winname="Sobels")
+
+    print "\t...done"
+    return orient_mask
 
 
-def drawLineFromPoint(img, c_point, angle_rad, length, two_dir=False):
+def drawLineFromPoint(img, c_point, angle_rad, length, color=255, two_dir=False):
     c_x, c_y = c_point
 
     x2_f = c_x + length * math.cos(angle_rad + np.pi / 2)
     y2_f = c_y + length * math.sin(angle_rad + np.pi / 2)
     x2 = int(round(x2_f))
     y2 = int(round(y2_f))
-    cv.line(img, (c_x, c_y), (x2, y2), 255)
+    cv.line(img, (c_x, c_y), (x2, y2), color)
 
     if two_dir:
         x3 = c_x + (-1) * (x2 - c_x)
         y3 = c_y + (-1) * (y2 - c_y)
-        cv.line(img, (c_x, c_y), (x3, y3), 255)
+        cv.line(img, (c_x, c_y), (x3, y3), color)
 #    return img
+
 
 def thinningGuoHallIteration(img, iter):
     marker = np.zeros(img.shape, np.uint8)
@@ -370,11 +377,13 @@ def calcCrossNumber(n_hood):
     return s
 
 
-def minutaeDetection(img):
+def minutaeDetection(img, postprocessing=True):
     print "Stage:\tMinutaes Detection"
-    drawed_img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    minutaes_figure = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    minutaes_figure = np.float64(minutaes_figure)
     img /= 255
-    minutaeMap = np.zeros(img.shape, np.uint8)
+    minutaeMap = np.zeros(img.shape, np.uint16)
+
     rc, cc = img.shape
     for r in range(1, rc - 1):
         for c in range(1, cc - 1):
@@ -395,80 +404,235 @@ def minutaeDetection(img):
             )
 
             cn = calcCrossNumber(nbhood)
-            if cn == 2 or cn == 6:
-                minutaeMap[r, c] = cn
-            if cn == 6:
-                cv.circle(drawed_img, (c, r), 4, (0, 0, 255))
-            if cn == 2:
-                cv.circle(drawed_img, (c, r), 4, (0, 255, 0))
 
+            # Bifurcation point
+            if cn == 6:
+                minutaeMap[r, c] = cn
+
+            # Termination point
+            if cn == 2:
+                minutaeMap[r, c] = cn
+
+
+    if not postprocessing:
+        print "\t...Minutiaes found: %d" % np.count_nonzero(minutaeMap)
+        print "\t...done"
+        return minutaeMap
+
+    # Minutiaes postprocessing
+    false_terminations = np.zeros(minutaeMap.shape, np.int16)
+
+    ns = 20
+    rc, cc = minutaeMap.shape
+
+    for r in range(ns, rc - ns):
+        for c in range(ns, cc - ns):
+            if minutaeMap[r, c] == 2:
+                nh_block = minutaeMap[r - ns: r + ns, c - ns: c + ns]
+                count_twos = 0
+                br_c, bc_c = nh_block.shape
+                for br in range(br_c):
+                    for bc in range(bc_c):
+                        if nh_block[br, bc] == 2:
+                            count_twos += 1
+                if count_twos < 4:
+                    false_terminations[r, c] = -2
+
+    minutaeMap = minutaeMap + false_terminations
+
+
+    print "\t...Minutiaes found: %d" % np.count_nonzero(minutaeMap)
     print "\t...done"
-    return drawed_img
+    return minutaeMap
+
+
+def drawMinutiaesOnImage(img, m_map, a_map):
+    print "Stage:\tDrawing minutiaes"
+    figure_img = np.copy(img)
+    figure_img = np.float32(img)
+    figure_img = cv.cvtColor(figure_img, cv.COLOR_GRAY2BGR)
+    rc, cc = m_map.shape
+    for r in range(rc):
+        for c in range(cc):
+            if m_map[r, c] == 2:
+                # Draw termination point
+                angle = a_map[r, c]
+                cv.circle(figure_img, (c, r), 4, (0, 255, 0))
+                drawLineFromPoint(figure_img, (c, r), angle, 7, color=(0, 0, 255), two_dir=True)
+            if m_map[r, c] == 6:
+                # Draw bifurcation point
+                angle = a_map[r, c]
+                cv.circle(figure_img, (c, r), 4, (0, 0, 255))
+                drawLineFromPoint(figure_img, (c, r), angle, 7, color=(0, 255, 0), two_dir=True)
+    print "\t...done"
+    return figure_img
+
+
+def calculateHoughTransform(m1, m2, fi_range, fi_tol, fi_err, dx_err, dy_err):
+    transforms = []
+
+    x_1, y_1, fi_1 = m1
+    x_2, y_2, fi_2 = m2
+
+    dx_err = int(dx_err)
+    dy_err = int(dy_err)
+
+    # Each value of rotation angle in range
+    for dfi in fi_range:
+        # Compute direction differnce
+        abs_dd = float(abs((fi_2 + dfi) - fi_1))
+        dd = min(abs_dd, 2.0 * np.pi - abs_dd)
+
+        if dd < fi_tol:
+            dx = x_1 - (math.cos(dfi) * x_2 - y_2 * math.sin(dfi))
+            dy = y_1 - (math.sin(dfi) * x_2 + y_2 * math.cos(dfi))
+
+            dx = int(round(dx))
+            dy = int(round(dy))
+
+            dfi_d = int(round(math.degrees(dfi)))
+
+            tr = (dx, dy, dfi_d)
+            transforms.append(tr)
+
+            # for e_x in range(dx - dx_err, dx + dx_err + 1):
+            #     for e_y in range(dy - dy_err, dy + dy_err + 1):
+            #         tr = (e_x, e_y, dfi_d)
+            #         transforms.append(tr)
+
+            # for e_fi in range(int(math.degrees(dfi - fi_err)), int(math.degrees(dfi + fi_err))):
+            #     for e_x in range(dx - dx_err, dx + dx_err + 1):
+            #         for e_y in range(dy - dy_err, dy + dy_err + 1):
+            #             tr = (e_x, e_y, e_fi)
+            #             transforms.append(tr)
+    return transforms
+
+
+def minutiaesMatching(m_set1, ms_set_2, fi_range=0, fi_tol=10, fi_err=0, dx_err=0, dy_err=0):
+    # Fi_range, fi_err, fi_tol sets in degrees
+    fi_range = [float(math.radians(f)) for f in fi_range]
+    fi_tol = float(math.radians(float(fi_tol)))
+    fi_err = float(math.radians(float(fi_err)))
+
+    accumulator = {}
+
+    print "Stage:\tMinutiaes matching"
+    print "\tSubstage: registration"
+
+    pair_counter = 0
+
+    for m1 in m_set1:
+        for m2 in ms_set_2:
+            pair_counter += 1
+            # print "\t\t_Pair #%d" % pair_counter
+            rates = calculateHoughTransform(m1, m2, fi_range, fi_tol, fi_err, dx_err, dy_err)
+            for rate in rates:
+                rate_key = str(rate)
+                if rate_key in accumulator.keys():
+                    accumulator[rate_key] += 1
+                else:
+                    accumulator[rate_key] = 1
+
+    ac_sorted = sorted(accumulator.items(), key=lambda x: x[1], reverse=True)
+
+    correct_transform = ac_sorted[0][0]
+
+    print "\tCorrect transform:"
+    print correct_transform
+    print "\t...done"
+
+
+def getMinutiesDescriptors(minutiaes_map, angles_map):
+    minutiae_set = []
+    rc, cc = minutiaes_map.shape
+    for r in range(rc):
+        for c in range(cc):
+            if minutiaes_map[r, c] != 0:
+                minutae = (c, r, angles_map[r, c])
+                minutiae_set.append(minutae)
+    return minutiae_set
+
+
+def filterImage(img):
+    segmented = morphoSegmentation(img)
+    normalized = normalization(segmented, d_mean=100, d_var=100)
+    binaryzed = shockAndBinarization(normalized)
+    thinned_gh = thinningGuoHall(binaryzed)
+    return thinned_gh
+
+
+def compareTwoFingerprints(img_1, img_2):
+
+    img_1 = imgToGray(img_1)
+    img_2 = imgToGray(img_2)
+
+    originals = np.concatenate((img_1, img_2), axis=1)
+    showImage(originals, winname="Fingerprint images")
+
+    print "\n"
+    print "=" * 20
+    print "Image 1:"
+    filtered_1 = filterImage(img_1)
+    min_map_1 = minutaeDetection(filtered_1)
+    angle_map_1 = orientionalComputing(filtered_1, step=1)
+    min_set_1 = getMinutiesDescriptors(min_map_1, angle_map_1)
+    # Extras: drawing
+    min_fig_1 = drawMinutiaesOnImage(img_1, min_map_1, angle_map_1)
+
+    print "\n"
+    print "Image 2:"
+    filtered_2 = filterImage(img_2)
+    min_map_2 = minutaeDetection(filtered_2)
+    angle_map_2 = orientionalComputing(min_map_2, step=1)
+    min_set_2 = getMinutiesDescriptors(min_map_2, angle_map_2)
+    # Extras: drawing
+    min_fig_2 = drawMinutiaesOnImage(img_2, min_map_2, angle_map_2)
+
+    print "\n"
+    print "Comparing two images:"
+    minutiaesMatching(min_set_1, min_set_2, fi_range=range(-2, 3), fi_tol=10, fi_err=1, dx_err=1, dy_err=1)
+
+    min_total_figure = np.concatenate((min_fig_1, min_fig_2), axis=1)
+    showImage(min_total_figure, winname="Minutaes drawed on fingerprints")
+
+    print "=" * 20
+    print "\n"
 
 
 def main():
     # Make a list of images stored in given folder
-    imgs = openImagesInFolder("./Fingerprints/DB4/")
+    imgs = openImagesInFolder("./Fingerprints/TEST_1/")
 
     # Files for sotring pathes to
     # good and bad examples of fingerprints
-    good_imgs = open("good.txt", 'w')
-    bad_imgs = open("bad.txt", 'w')
+    # good_imgs = open("good.txt", 'w')
+    # bad_imgs = open("bad.txt", 'w')
 
-    img_counter = 1
+    compareTwoFingerprints(imgs[0][0], imgs[1][0])
 
-    # Loop this images
-    for img_t in imgs:
+    # Wait for any key before switch to another image
+    k_code = getKey(delay=0)
 
-        img = img_t[0]
-
-        print "="*20
-        print "Image %d: %s" % (img_counter, img_t[1])
-
-        # Show original image to comapare transitions
-        showImage(img, winname="Original. Size: (%d, %d, %d)" % img.shape)
-
-        # Convert image to grayscale
-        grayed = imgToGray(img)
-
-        segmented = morphoSegmentation(grayed)
-
-        normalized = normalization(segmented, d_mean=100, d_var=100)
-
-        binaryzed = shockAndBinarization(normalized)
-
-        orientionalComputing(binaryzed)
-#        thinned_gh = thinningGuoHall(binaryzed)
-
- #       minutaesMap = minutaeDetection(thinned_gh)
-  #      showImage(minutaesMap, winname="Map")
-
-        # writeImg(thinned, folder="./Fingerprints/DB2/Thinned/")
-
-        img_counter += 1
-
-        # Wait for any key before switch to another image
-        k_code = getKey(delay=0)
-
-        # G key was pressed:
-        if k_code == 103:
-            try:
-                good_imgs.write(img_t[1] + "\n")
-            except Exception:
-                print "Error while writing GOOD image path"
-                continue
-        # B key was pressed:
-        elif k_code == 98:
-            try:
-                bad_imgs.write(img_t[1] + "\n")
-                os.remove(img_t[1])
-            except Exception:
-                print "Erorr while writing BAD image path"
-        # ESC was pressed:
-        elif k_code == 27:
-            break
-        else:
-            continue
+    # G key was pressed:
+    if k_code == 103:
+        try:
+            good_imgs.write(img_t[1] + "\n")
+        except Exception:
+            print "Error while writing GOOD image path"
+            # continue
+    # B key was pressed:
+    # elif k_code == 98:
+    #     try:
+    #         bad_imgs.write(img_t[1] + "\n")
+    #         os.remove(img_t[1])
+    #     except Exception:
+    #         print "Erorr while writing BAD image path"
+    # ESC was pressed:
+    elif k_code == 27:
+        # break
+        return
+    # else:
+        # continue
 
 
 if __name__ == "__main__":
